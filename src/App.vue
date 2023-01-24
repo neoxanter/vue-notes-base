5067e532bfaa8e5293c5f51fffbe37eea5ce3f7c
Serg Mark <sgcont@gmail.com>
2023-01-24 11:49:36 +0300

    Added svg icons

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,49 @@
     <div class="wrapper-content">
       <section>
         <div class="container">
-          <h1>{{ title }}</h1>
-
           <message v-if="message" :message="message" />
           <newNote :note="note" @addNote="addNote" />
+
+          <div class="note-header">
+            <h1>{{ title }}</h1>
+            <div class="icons">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <rect x="3" y="3" width="7" height="7"></rect>
+                <rect x="14" y="3" width="7" height="7"></rect>
+                <rect x="14" y="14" width="7" height="7"></rect>
+                <rect x="3" y="14" width="7" height="7"></rect>
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <line x1="8" y1="6" x2="21" y2="6"></line>
+                <line x1="8" y1="12" x2="21" y2="12"></line>
+                <line x1="8" y1="18" x2="21" y2="18"></line>
+                <line x1="3" y1="6" x2="3" y2="6"></line>
+                <line x1="3" y1="12" x2="3" y2="12"></line>
+                <line x1="3" y1="18" x2="3" y2="18"></line>
+              </svg>
+            </div>
+          </div>
+
           <notes :notes="notes" @remove="removeNote" />
 
           <div class="message" v-if="message">
--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -49,9 +49,20 @@ export default {
   display: flex;
   align-items: center;
   justify-content: space-between;
+  h1 {
+    font-size: 32px;
+  }
   p {
     font-style: 22px;
     color: rgb(59, 24, 212);
+  }
+
+  svg {
+    margin-right: 12px;
+    color: #999;
+    &:last-child {
+      margin-right: 0;
+    }
   }
 }
 .note-body {
